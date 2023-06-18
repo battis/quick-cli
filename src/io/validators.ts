c6699cd7f7b77bc93cad99c2ac1034df9e2d3f60
Seth Battis <sbattis@groton.org>
2023-06-18 15:43:08 -0400

    Include guidance in validators

--- src/io/validators.ts
+++ src/io/validators.ts
@@ -1,15 +1,20 @@
 import emailValidator from 'email-validator';
 
-export const notEmpty = (value?: string) => value && value.length > 0;
+export const notEmpty = (value?: string) =>
+  (!!value && value.length > 0) || 'May not be empty';
 
 export const email = (value?: string) =>
-  notEmpty(value) && emailValidator.validate(value || '');
+  (notEmpty(value) && emailValidator.validate(value || '')) ||
+  'Must be valid email address';
 
 export const minLength = (minLength: number) => (value?: string) =>
-  value && value.length >= minLength;
+  (!!value && value.length >= minLength) ||
+  `Must be at least ${minLength} characters`;
 
 export const maxLength = (maxLength: number) => (value?: string) =>
-  value && value.length <= maxLength;
+  (!!value && value.length <= maxLength) ||
+  `Must be no more than ${maxLength} characters`;
 
 export const lengthBetween = (min: number, max: number) => (value?: string) =>
-  minLength(min)(value) && maxLength(max)(value);
+  (minLength(min)(value) && maxLength(max)(value)) ||
+  `Must be between ${minLength} && ${maxLength} characters`;
