c257fa22f5e218b708c632b82325bd3d070d88d5
Seth Battis <seth@battis.net>
2023-12-06 10:08:23 -0500

    modularize validators

--- src/validators.ts
+++ src/validators.ts
@@ -6,84 +6,110 @@ import pathValidator from 'is-valid-path';
 import path from 'path';
 import core from './core';
 
-const notEmpty = (value?: string) =>
-  (!!value && value.length > 0) || 'May not be empty';
+export type Validator = (value?: string) => boolean | string;
 
-const minLength = (minLength: number) => (value?: string) =>
-  (!!value && value.length >= minLength) ||
-  `Must be at least ${minLength} characters`;
+export function notEmpty(value?: string) {
+  return (!!value && value.length > 0) || 'May not be empty';
+}
 
-const maxLength = (maxLength: number) => (value?: string) =>
-  (!!value && value.length <= maxLength) ||
-  `Must be no more than ${maxLength} characters`;
+export function minLength(minLength: number): Validator {
+  return (value?: string) =>
+    (!!value && value.length >= minLength) ||
+    `Must be at least ${minLength} characters`;
+}
 
-const isPath = (value?: string) =>
-  (notEmpty(value) && pathValidator(value)) || 'Must be a valid path';
+export function maxLength(maxLength: number): Validator {
+  return (value?: string) =>
+    (!!value && value.length <= maxLength) ||
+    `Must be no more than ${maxLength} characters`;
+}
+
+export function isPath(value?: string) {
+  return (notEmpty(value) && pathValidator(value)) || 'Must be a valid path';
+}
+
+export function match(pattern: RegExp): Validator {
+  return (value?: string) =>
+    (!!value && pattern.test(value)) ||
+    `Must match pattern /${pattern.toString()}/`;
+}
+
+export function lengthBetween(min: number, max: number): Validator {
+  return (value?: string) =>
+    (minLength(min)(value) && maxLength(max)(value)) ||
+    `Must be between ${minLength} && ${maxLength} characters`;
+}
+
+export function email(value?: string) {
+  return (
+    (notEmpty(value) && emailValidator.validate(value || '')) ||
+    'Must be valid email address'
+  );
+}
+
+export function cron(value?: string) {
+  return (
+    (notEmpty(value) && cronValidator(value || '').isValid()) ||
+    'Must be valid cron schedule'
+  );
+}
+
+export function isHostname({
+  subdomain = false,
+  wildcard = false,
+  allowUnicode = false,
+  topLevel = false,
+  localhost = true,
+  ipAddress = true,
+  allowed = []
+}): Validator {
+  return (value?: string) => {
+    const domain = domainValidator(value, {
+      allowSubdomain: subdomain,
+      allowWildcard: wildcard,
+      allowIdn: allowUnicode,
+      checkTld: topLevel
+    });
+    return (
+      (!!value &&
+        (domain.result ||
+          (localhost && value === 'localhost') ||
+          (ipAddress && /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.test(value)) ||
+          allowed.includes(value))) ||
+      `Must be a valid hostname${domain.result ? '' : ` (${domain.message})`}`
+    );
+  };
+}
+
+export function pathExists(root = core.appRoot()): Validator {
+  return function (value?: string) {
+    const possiblePath = path.resolve(root, value || '');
+    return (
+      (isPath(value) && fs.existsSync(possiblePath)) ||
+      `${possiblePath} does not exist`
+    );
+  };
+}
+
+export function combine(...validators: (Validator | undefined)[]): Validator {
+  return (value?: string) =>
+    validators.reduce(
+      (valid, validator?: Validator) =>
+        validator ? (valid && validator ? validator(value) : true) : valid,
+      true
+    );
+}
 
 export default {
   notEmpty,
   minLength,
   maxLength,
   isPath,
-
-  match: (pattern: RegExp) => (value?: string) =>
-    (!!value && pattern.test(value)) ||
-    `Must match pattern /${pattern.toString()}/`,
-
-  lengthBetween: (min: number, max: number) => (value?: string) =>
-    (minLength(min)(value) && maxLength(max)(value)) ||
-    `Must be between ${minLength} && ${maxLength} characters`,
-
-  email: (value?: string) =>
-    (notEmpty(value) && emailValidator.validate(value || '')) ||
-    'Must be valid email address',
-
-  cron: (value?: string) =>
-    (notEmpty(value) && cronValidator(value || '').isValid()) ||
-    'Must be valid cron schedule',
-
-  isHostname:
-    ({
-      subdomain = false,
-      wildcard = false,
-      allowUnicode = false,
-      topLevel = false,
-      localhost = true,
-      ipAddress = true,
-      allowed = []
-    }) =>
-    (value?: string) => {
-      const domain = domainValidator(value, {
-        allowSubdomain: subdomain,
-        allowWildcard: wildcard,
-        allowIdn: allowUnicode,
-        checkTld: topLevel
-      });
-      return (
-        (!!value &&
-          (domain.result ||
-            (localhost && value === 'localhost') ||
-            (ipAddress && /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.test(value)) ||
-            allowed.includes(value))) ||
-        `Must be a valid hostname${domain.result ? '' : ` (${domain.message})`}`
-      );
-    },
-
-  pathExists: (root = core.appRoot()) =>
-    function (value?: string) {
-      const possiblePath = path.resolve(root, value || '');
-      return (
-        (isPath(value) && fs.existsSync(possiblePath)) ||
-        `${possiblePath} does not exist`
-      );
-    },
-
-  combine:
-    (...validators: (((value?: string) => boolean | string) | undefined)[]) =>
-    (value?: string) =>
-      validators.reduce(
-        (valid, validator) =>
-          validator ? (valid && validator ? validator(value) : true) : valid,
-        true
-      )
+  match,
+  lengthBetween,
+  email,
+  cron,
+  isHostname,
+  pathExists,
+  combine
 };
