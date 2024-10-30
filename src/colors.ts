cfd9614936bc14ee8b97e55c03496106c2b653ab
Seth Battis <seth@battis.net>
2024-10-30 15:23:45 -0400

    node-style colors

--- src/colors.ts
+++ src/colors.ts
@@ -1,8 +1,9 @@
 import chalk from 'chalk';
 
 export default {
-  value: chalk.cyan,
-  url: chalk.yellow,
+  value: chalk.yellow,
+  quotedValue: chalk.green,
+  url: chalk.cyan,
   error: chalk.bold.red,
   command: chalk.redBright
 };
