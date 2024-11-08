0786f21d16e2ad36a14ad88828a62cc2fb43e9b8
Seth Battis <seth@battis.net>
2024-11-08 08:06:32 -0700

    regexpValue

--- src/colors.ts
+++ src/colors.ts
@@ -3,6 +3,7 @@ import chalk from 'chalk';
 export default {
   value: chalk.yellow,
   quotedValue: chalk.green,
+  regexpValue: chalk.red,
   url: chalk.cyan,
   error: chalk.bold.red,
   command: chalk.redBright
