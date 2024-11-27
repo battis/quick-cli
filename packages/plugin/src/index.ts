5f7d78f6d615f28e076ab497e4bc75a73a2724d5
Seth Battis <seth@battis.net>
2024-11-27 13:14:54 -0500

    document hooks

--- packages/plugin/src/index.ts
+++ packages/plugin/src/index.ts
@@ -6,9 +6,17 @@ export { Arguments, Options };
 export abstract class Base {
   public constructor(public readonly name: string) {}
 
+  /**
+   * Override to include custom Jackspeak configuration.
+   *
+   * Will be called _before_ init() has been called.
+   */
   public options(): Options {
     return {};
   }
 
+  /**
+   * Override to process user-provided arguments parsed by Jackspeak
+   */
   public init(args: Arguments<ReturnType<this['options']>>): void {}
 }
